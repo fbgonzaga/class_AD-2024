5220e86b4832fd6eba4451ac21cf3eaf5c997639
Flavio Gonzaga <fbgonzaga@gmail.com>
2024-08-27 12:10:50 -0300

    Developed the simulation, incorporating arrival and service times based on an exponential distribution. Added utilization measurement to this version of the simulation.

--- simulacao.c
+++ simulacao.c
@@ -12,7 +12,7 @@ double uniforme(){
     return u;
 }
 
-double gera_chegada(double l){
+double gera_tempo(double l){
     return (-1.0/l) * log(uniforme());
 }
 
@@ -22,19 +22,31 @@ double min(double n1, double n2){
 }
 
 int main(){
-    double l;
-    printf("Informe o tempo médio entre as chegadas: ");
-    scanf("%lF", &l);
-    l = 1.0/l;
+    srand(time(NULL));
+    double parametro_chegada;
+    printf("Informe o tempo médio entre as chegadas (s): ");
+    scanf("%lF", &parametro_chegada);
+    parametro_chegada = 1.0/parametro_chegada;
+
+    double parametro_saida;
+    printf("Informe o tempo médio de atendimento (s): ");
+    scanf("%lF", &parametro_saida);
+    parametro_saida = 1.0/parametro_saida;
 
     double tempo_simulacao;
-    printf("Informe o tempo de simulacao: ");
+    printf("Informe o tempo de simulacao (s): ");
     scanf("%lF", &tempo_simulacao);
 
     double tempo_decorrido = 0.0;
 
-    double tempo_chegada = gera_chegada(l);
+    double tempo_chegada = gera_tempo(parametro_chegada);
     double tempo_saida = DBL_MAX;
+
+    unsigned long int fila = 0;
+    unsigned long int fila_max = 0;
+
+    double soma_ocupacao = 0.0;
+
 
     while(tempo_decorrido <= tempo_simulacao){
         tempo_decorrido = 
@@ -42,11 +54,40 @@ int main(){
 
         //chegada
         if(tempo_decorrido == tempo_chegada){
+            //sistema esta ocioso?
+            if(!fila){
+                tempo_saida =
+                tempo_decorrido +
+                gera_tempo(parametro_saida);
 
+                soma_ocupacao += tempo_saida - 
+                tempo_decorrido;
+            }
+            fila++;
+            fila_max = fila > fila_max?
+                fila:
+                fila_max;
+
+            tempo_chegada =
+            tempo_decorrido +
+            gera_tempo(parametro_chegada);
         }else{
-            //saida
+            fila--;
+            tempo_saida = DBL_MAX;
+            //tem mais requisicoes na fila?
+            if(fila){
+                tempo_saida =
+                tempo_decorrido +
+                gera_tempo(parametro_saida);
+
+                soma_ocupacao += tempo_saida - 
+                tempo_decorrido;
+            }
         }
     }
+
+    printf("Maior tamanho de fila alcancado: %d\n",fila_max);
+    printf("Ocupacao: %lF\n",soma_ocupacao/tempo_decorrido);
 
     return 0;
 }
